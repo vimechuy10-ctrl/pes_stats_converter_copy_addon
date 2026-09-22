e2b20468669de3d583716f78b4ce10cffbf57036
vimechuy10-ctrl <vimechuy10@gmail.com>
2026-09-22 14:58:49 -0500

    boton flotante en Fminside
    cambio para soporte del boton flotante en fninside que no aparecia

--- src/content/sources/fminside.js
+++ src/content/sources/fminside.js
@@ -410,7 +410,7 @@ window.PESConverter.registerSource({
     /**
      * @returns {boolean} True if inside a player page.
      */
-    return /^\/players\/\d+-[^/]+\/\d+-[^/]+$/.test(
+    return /^\/players\/\d+-fm-\d+\/\d+-[^/]+$/.test(
       window.location.pathname,
     );
   },
